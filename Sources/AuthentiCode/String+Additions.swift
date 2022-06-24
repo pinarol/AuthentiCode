7098b89f2c17d9b98603550d0aa1e73e02542ddb
pinar olguc <pinarolguc@gmail.com>
2022-06-24 18:58:16 +0300

    Remove unused

--- Sources/AuthentiCode/String+Additions.swift
+++ Sources/AuthentiCode/String+Additions.swift
@@ -1,22 +1,6 @@
 import Foundation
 
 extension String {
-    var hexData: Data? {
-        var data = Data(capacity: count / 2)
-        var indexIsEven = true
-        for i in self.indices {
-            if indexIsEven {
-                let byteString = self[i...self.index(after: i)]
-                if var num = UInt8(byteString, radix: 16) {
-                    data.append(&num, count: 1)
-                } else {
-                    return nil
-                }
-            }
-            indexIsEven.toggle()
-        }
-        return data
-    }
     
     var hexString: String? {
         let data = Data(utf8)
