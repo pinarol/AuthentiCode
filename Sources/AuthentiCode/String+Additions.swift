0d70a8c93361570bd048d690e33a6bd35f903da2
pinar olguc <pinarolguc@gmail.com>
2022-07-02 22:00:14 +0300

    Open hexString

--- Sources/AuthentiCode/String+Additions.swift
+++ Sources/AuthentiCode/String+Additions.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 extension String {
-    
-    var hexString: String? {
+    /// HEX representation of given String's bytes.
+    public var hexString: String? {
         let data = Data(utf8)
         let hexString = data.map{ String(format:"%02x", $0) }.joined()
         return hexString
